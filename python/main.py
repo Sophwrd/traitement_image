7a553852312c3179db8abadfb97e8ea7f886ac4a
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2026-05-16 14:04:37 +0200

    Update main.py

--- python/main.py
+++ python/main.py
@@ -14,21 +14,23 @@ import cv2
 Convert to gray for opencv bc it expects gray images
 Source: https://www.superdatascience.com/blogs/opencv-face-detection
 """
-def convert_to_rgb(img):
-    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def convert_to_rgb(img): # cv loads images into bgr by default
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # convert images to different color
 
 # loading up cascade classifier training file for haarcascade
 haar_face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
 
 """  
-The code is pretty much the same as before; it’s just grouped inside a 
-function for reusability detect faces
+Face detection grouped in a function
 """
 def detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
     img_copy = colored_img.copy()
 
     gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
-
+    
+    # multiscale: scans and detect faces + returns points of face in rect(x,y,w,h)
+    # scaleFactor: compensates when 1 face appears bigger than others
+    # minNeighbors: moving windows to detect
     faces = f_cascade.detectMultiScale(gray, scaleFactor=scaleFactor, minNeighbors=5)
     print('Faces found: ', len(faces)) # give the number of faces
 
@@ -36,7 +38,9 @@ def detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
         cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
     return img_copy
 
-#blur effect
+"""  
+Blur effect on a detected face
+"""
 def blur_detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
     img_copy = colored_img.copy()
 
@@ -46,12 +50,12 @@ def blur_detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
     #print('Faces found: ', len(faces))
 
     #https://www.geeksforgeeks.org/python/how-to-blur-faces-in-images-using-opencv-in-python/
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in faces: # loop over faces
         cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
         #blur effect
-        roi = img_copy[y:y+h, x:x+w]
+        roi = img_copy[y:y+h, x:x+w] # extracts face region
         roi = cv2.GaussianBlur(roi, (75, 75), 30) # here to change the intensity of the blur
-        img_copy[y:y+h, x:x+w] = roi
+        img_copy[y:y+h, x:x+w] = roi # replaces original face with blurred face
     return img_copy
 
 """ Cours de traitment d'image
@@ -65,7 +69,7 @@ for file_name in Path(INPUT_DIR).iterdir():
         img = cv2.imread(str(file_name))
        # blurring of the face
         faces_blurred_img = blur_detect_faces(haar_face_cascade, img)
-        plt.imshow(convert_to_rgb(faces_blurred_img))
+        plt.imshow(convert_to_rgb(faces_blurred_img)) # display image
         plt.show()
 
        # face detection
