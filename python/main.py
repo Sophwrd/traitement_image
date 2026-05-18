a6da00247cc8cafda715c902966ce670d029427d
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2026-05-18 16:03:45 +0200

    Ajout des docstrings

--- python/main.py
+++ python/main.py
@@ -15,15 +15,30 @@ Convert to gray for opencv bc it expects gray images
 Source: https://www.superdatascience.com/blogs/opencv-face-detection
 """
 def convert_to_rgb(img): # cv loads images into bgr by default
+    """Converts image colours from BGR to RGB
+
+    Args:
+        img : image in BGR colour
+
+    Returns:
+        Return the image in RGB colours
+    """
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # convert images to different color
 
 # loading up cascade classifier training file for haarcascade
 haar_face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
 
-"""  
-Face detection grouped in a function
-"""
 def detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
+    """Face detection grouped in a function
+
+    Args:
+        f_cascade (cv2.CascadeClassifier): Haar face classifier used for face detection
+        colored_img : image in BGR colours
+        scaleFactor (float): 1.1, compensates when 1 face appears bigger than others
+
+    Returns:
+        Return the image with the detected face in a rectangle
+    """
     img_copy = colored_img.copy()
 
     gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
@@ -38,10 +53,17 @@ def detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
         cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
     return img_copy
 
-"""  
-Blur effect on a detected face
-"""
 def blur_detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
+    """Blur effect on a detected face
+
+    Args:
+        f_cascade (cv2.CascadeClassifier): Haar face classifier used for face detection
+        colored_img : image in BGR colours
+        scaleFactor (float): 1.1, compensates when 1 face appears bigger than others
+
+    Returns:
+        Return the image with blurred detected faces in a rectangle
+    """
     img_copy = colored_img.copy()
 
     gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
